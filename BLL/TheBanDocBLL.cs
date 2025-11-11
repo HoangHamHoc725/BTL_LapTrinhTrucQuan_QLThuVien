f7c38eb9cb84c2f497a64acea356743586dd8510
Harry <hoangnguyen72.study@gmail.com>
2025-11-11 14:47:26 +0700

    init func TheBanDoc

---
+++ BLL/TheBanDocBLL.cs
@@ -0,0 +1,31 @@
+﻿using LibraryManagerApp.DAL;
+using LibraryManagerApp.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagerApp.BLL
+{
+    internal class TheBanDocBLL
+    {
+        private TheBanDocDAL _dal = new TheBanDocDAL();
+        private BanDocDAL _bdDal = new BanDocDAL();
+
+        public List<TheBanDocDTO> LayThongTinTheBanDoc()
+        {
+            return _dal.GetAllTheBanDocDTO();
+        }
+
+        public TheBanDocDTO LayChiTietTheBanDoc(string maTBD)
+        {
+            return _dal.GetTheBanDocByMaTBD(maTBD);
+        }
+
+        public List<BanDocChuaCoTheDTO> LayBanDocChuaCoThe()
+        {
+            return _bdDal.GetBanDocChuaCoThe();
+        }
+    }
+}
---
+++ DAL/TheBanDocDAL.cs
@@ -0,0 +1,47 @@
+﻿using LibraryManagerApp.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagerApp.DAL
+{
+    internal class TheBanDocDAL
+    {
+        // Hàm READ chính: Lấy tất cả Thẻ Bạn Đọc kèm thông tin liên quan
+        public List<TheBanDocDTO> GetAllTheBanDocDTO()
+        {
+            using (var db = new QLThuVienDataContext())
+            {
+                // Truy vấn phức tạp: JOIN tTheBanDoc, tBanDoc, tTaiKhoan, tNhanVien
+                var query = from tbd in db.tTheBanDocs
+                            join bd in db.tBanDocs on tbd.MaBD equals bd.MaBD
+                            join tk in db.tTaiKhoans on tbd.MaTK equals tk.MaTK
+                            join nv in db.tNhanViens on tk.MaNV equals nv.MaNV
+                            select new TheBanDocDTO
+                            {
+                                MaTBD = tbd.MaTBD,
+                                MaBD = tbd.MaBD,
+                                MaTK = tbd.MaTK,
+                                NgayCap = tbd.NgayCap,
+                                NgayHetHan = tbd.NgayHetHan,
+                                TrangThai = tbd.TrangThai,
+
+                                HoTenBD = bd.HoDem + " " + bd.Ten,
+                                HoTenNV = nv.HoDem + " " + nv.Ten
+                            };
+
+                return query.ToList();
+            }
+        }
+
+        // Hàm READ Chi tiết (dùng cho CellClick)
+        public TheBanDocDTO GetTheBanDocByMaTBD(string maTBD)
+        {
+            // Tối ưu: Chỉ truy vấn bản ghi cụ thể nếu có nhiều dữ liệu. 
+            // Hiện tại, ta dùng GetAllTheBanDocDTO().SingleOrDefault() để đơn giản
+            return GetAllTheBanDocDTO().SingleOrDefault(p => p.MaTBD == maTBD);
+        }
+    }
+}
