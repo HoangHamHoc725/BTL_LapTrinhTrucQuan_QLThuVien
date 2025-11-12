fe72c4058f3be371e97e459d2a63bc8d6e6723ca
Harry <hoangnguyen72.study@gmail.com>
2025-11-12 14:34:07 +0700

    update 1: muontra

--- GUI/UserControls/QLMuonTra/ucFrmQuanLyMuonTra.cs
+++ GUI/UserControls/QLMuonTra/ucFrmQuanLyMuonTra.cs
@@ -26,39 +26,5 @@ namespace LibraryManagerApp.GUI.UserControls.QLMuonTra
             this.pnlContent.Controls.Add(uc);
             uc.BringToFront();
         }
-
-        private void btnThongTinPhieuMuon_Click(object sender, EventArgs e)
-        {
-            LoadSubUserControl(new ucFrmThongTinPhieuMuon());
-            SetActiveButton(btnThongTinPhieuMuon);
-        }
-
-        private void btnThongTinPhieuTra_Click(object sender, EventArgs e)
-        {
-            LoadSubUserControl(new ucFrmThongTinPhieuTra());
-            SetActiveButton(btnThongTinPhieuTra);
-        }
-
-        private void ucFrmQuanLyMuonTra_Load(object sender, EventArgs e)
-        {
-            btnThongTinPhieuMuon_Click(btnThongTinPhieuMuon, EventArgs.Empty);
-        }
-
-        private void SetActiveButton(Button activeButton)
-        {
-            string newButtonTag = activeButton.Tag as string;
-
-            if (currentActiveButton != null)
-            {
-                // Reset trạng thái nút cũ
-                currentActiveButton.BackColor = Color.FromArgb(48, 52, 129);
-                currentActiveButton.ForeColor = Color.FromArgb(245, 245, 245);
-            }
-
-            currentActiveButton = activeButton;
-
-            currentActiveButton.BackColor = Color.FromArgb(37, 40, 106);
-            currentActiveButton.ForeColor = Color.FromArgb(255, 242, 0);
-        }
     }
 }
